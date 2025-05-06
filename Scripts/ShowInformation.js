b2d1c57bd77c0971ade8deff4cdb739999020376
Vidar <vidal08a@edu.tidaholm.se>
2025-05-06 15:15:41 +0200

    fortsatt jobba

---
+++ Scripts/ShowInformation.js
@@ -0,0 +1,9 @@
+function showInfo() {
+	var Info = document.getElementByClassName("Information");
+	if (Info.style.display === "none") {
+		Info.style.display = "block";
+	}
+	else  {
+		Info.style.display = "none";
+	}
+}
