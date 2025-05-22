280d0342025d960f3e21f0f7baaa85e4f4fcd0eb
Vidar <vidal08a@edu.tidaholm.se>
2025-05-22 12:44:31 +0200

    Added javascript in Triassic file

--- Scripts/ShowInformation.js
+++ Scripts/ShowInformation.js
@@ -1,9 +1,20 @@
-function showInfo() {
-	var Info = document.getElementByClassName("Information");
-	if (Info.style.display === "none") {
-		Info.style.display = "block";
-	}
-	else  {
-		Info.style.display = "none";
-	}
-}
+$(document).ready(function(){
+    $('.ExpandButton').click(function() {
+                // Hitta målet (informationsdiven) baserat på knappens data-target attribut
+        var targetId = $(this).data('target');
+        var $infoDiv = $('#' + targetId);
+
+                // Växla synlighet för informationsdiven
+        $infoDiv.slideToggle(400); // 400 är animationstiden i millisekunder
+
+                // Ändra knapptexten baserat på tillstånd
+		if ($(this).text() === "Show Info") 
+		{
+			$(this).text("Hide Info");
+		}
+		else 
+		{
+			$(this).text("Show Info"); // Om den var synlig och nu är dold
+		}
+    });
+});
